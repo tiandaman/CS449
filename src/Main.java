d16f6055e040167f445768346bac481097a09d70
tiandaman <52509578+tiandaman@users.noreply.github.com>
2023-03-19 17:12:34 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main extends GUI {
+    public static void main(String[] args) {
+
+        GUI myGUI = new GUI();
+        while (!myGUI.checkSimpleGameWinner()) {
+        }
+    }
+}
+
+
